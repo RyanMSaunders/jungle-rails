34ac224729f10fead026da6c22182d348c9221f3
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-07-25 14:57:58 -0400

    Add styling to user registration and login. Update styling to About link bottom nav.

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
  # Login and logout routes
  get '/login', to: 'sessions#new', as: 'login'
  post '/login', to: 'sessions#create'
- delete '/logout', to: 'sessions#destroy', as: 'logout'
+ get '/logout', to: 'sessions#destroy', as: 'logout'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
